740c1f04e7549a73677c815c07f35ef5e19447ec
rafi.hariman <231232609@uii.ac.id>
2025-01-06 20:06:07 +0700

    Remove deprecated Vercel configuration and image API implementation

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-// const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
 
-// const app = express();
-// const PORT = 3000;
+const app = express();
+const PORT = 3000;
 
-// app.get('/api/image/:filename', (req, res) => {
-//   const { filename } = req.params;
-//   const imagePath = path.join(__dirname, 'assets', 'img', 'gallery', filename);
+app.get('/api/image/:filename', (req, res) => {
+  const { filename } = req.params;
+  const imagePath = path.join(__dirname, 'assets', 'img', 'gallery', filename);
 
-//   res.sendFile(imagePath, (err) => {
-//     if (err) {
-//       res.status(404).send('Image not found');
-//     }
-//   });
-// });
+  res.sendFile(imagePath, (err) => {
+    if (err) {
+      res.status(404).send('Image not found');
+    }
+  });
+});
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
